4a1e3387ee18beb982815ab6f7a2ad7dfe1e2550
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2020-01-03 00:31:40 +0100

    Modified app.js

--- server/app.js
+++ server/app.js
@@ -32,10 +32,10 @@ app.use(
   })
 );
 
-app.use(express.static(path.resolve("../client/build/")));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("/client/build/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 4000;
---
+++ server/build/precache-manifest.e2fe08b651714ad5db3550b103f49f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff10ac7c20920451af4415f0efa1fdc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ce35d09b9644feecaae",
+    "url": "/static/css/main.f5f53b8d.chunk.css"
+  },
+  {
+    "revision": "6ffbd2af542896907263",
+    "url": "/static/js/2.5c7d82b4.chunk.js"
+  },
+  {
+    "revision": "fd56044fbab90a74509299bbf81211c5",
+    "url": "/static/js/2.5c7d82b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ce35d09b9644feecaae",
+    "url": "/static/js/main.a1f2c0d4.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
